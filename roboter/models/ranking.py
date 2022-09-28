3382c181a00591df6522810212d795d48aeb4a20
KOJI-YAMABE <kvbl.ak25@gmail.com>
2022-09-28 23:25:14 +0900

    first commit

---
+++ roboter/models/ranking.py
@@ -0,0 +1,76 @@
+import collections
+import csv
+import os
+import pathlib
+
+RANKING_COLUMN_USER_NAME = 'USER_NAME'
+RANKING_COLUMN_MENU = 'MENU'
+RANKING_COLUMN_COUNT = 'COUNT'
+RANKING_CSV_FILE_PATH = 'ranking.csv'
+
+
+class CsvModel(object):
+    def __init__(self, csv_file):
+        self.csv_file = csv_file
+        if not os.path.exists(csv_file):
+            pathlib.Path(csv_file).touch()
+
+
+class RankingModel(CsvModel):
+    def __init__(self, csv_file=None, *args, **kwargs):
+        if not csv_file:
+            csv_file = self.get_csv_file_path()
+        super().__init__(csv_file, *args, **kwargs)
+        self.column = [RANKING_COLUMN_MENU, RANKING_COLUMN_COUNT]
+        self.data = collections.defaultdict(int)
+        self.load_data()
+
+    def get_most_popular(self, not_list=None):
+
+        if not_list is None:
+            not_list = []
+
+        if not self.data:
+            return None
+
+        sorted_data = sorted(self.data, key=self.data.get, reverse=True)
+        for restaurant_name in sorted_data:
+            if restaurant_name in not_list:
+                continue
+            return restaurant_name
+
+    def get_csv_file_path(self):
+        csv_file_path = None
+        try:
+            import settings
+            if settings.CSV_FILE_PATH:
+                csv_file_path = settings.CSV_FILE_PATH
+        except ImportError:
+            pass
+
+        if not csv_file_path:
+            csv_file_path = RANKING_CSV_FILE_PATH
+        return csv_file_path
+
+    def load_data(self):
+        with open(self.csv_file, 'r+') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                self.data[row[RANKING_COLUMN_MENU]] = int(row[RANKING_COLUMN_COUNT])
+        return self.data
+
+    def increment(self, menu, user_name):
+        self.data[menu.title()] += 1
+        self.save(user_name)
+
+    def save(self, user_name):
+        with open(self.csv_file, 'w+') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self.column)
+            writer.writeheader()
+            # writer.writerow(RANKING_COLUMN_USER_NAME: user_name)
+
+            for menu, count in self.data.items():
+                writer.writerow({
+                    RANKING_COLUMN_MENU: menu,
+                    RANKING_COLUMN_COUNT: count
+                })
---
+++ roboter/models/robot.py
@@ -0,0 +1,74 @@
+import csv
+
+from roboter.views import console
+from roboter.models import ranking
+
+DEFAULT_NAME = '基山'
+
+class Robot(object):
+    def __init__(self, restaurant_name=DEFAULT_NAME, user_name='', speaker_color='green'):
+        self.restaurant_name = restaurant_name
+        self.user_name = user_name
+        self.speaker_color = speaker_color
+
+    def hello(self):
+        while True:
+            template = console.get_template('hello.txt', self.speaker_color)
+            user_name = input(template.substitute({'restaurant_name': self.restaurant_name}))
+            if user_name:
+                self.user_name = user_name.title()
+                break
+
+
+class RestaurantRobot(Robot):
+    def __init__(self, restaurant_name=DEFAULT_NAME):
+        super().__init__(restaurant_name=restaurant_name)
+        self.ranking_model = ranking.RankingModel()
+
+    def _hello_decorator(func):
+        def wrapper(self):
+            if not self.user_name:
+                self.hello()
+            return func(self)
+        return wrapper
+
+    @_hello_decorator
+    def recommend_menu(self):
+        new_recommend_menu = self.ranking_model.get_most_popular()
+        if not new_recommend_menu:
+            return None
+
+        will_recommend_menus = [new_recommend_menu]
+        while True:
+            template = console.get_template('greeting.txt', self.speaker_color)
+            is_yes = input(template.substitute({
+                'restaurant_name': self.restaurant_name,
+                'user_name': self.user_name,
+                'menu': new_recommend_menu
+            }))
+
+            if is_yes.lower() == 'y' or is_yes.lower() == 'yes':
+                break
+
+            if is_yes.lower() == 'n' or is_yes.lower() == 'no':
+                new_recommend_menu = self.ranking_model.get_most_popular(not_list=will_recommend_menus)
+                if not new_recommend_menu:
+                    break
+                will_recommend_menus.append(new_recommend_menu)
+
+    @_hello_decorator
+    def ask_your_favorite(self):
+        while True:
+            template = console.get_template('which_menu.txt', self.speaker_color)
+            menu = input(template.substitute({
+                'user_name': self.user_name
+            }))
+            if menu:
+                self.ranking_model.increment(menu, self.user_name)
+                break
+
+    def thank_you(self):
+        template = console.get_template('good_by.txt', self.speaker_color)
+        print(template.substitute({
+            'user_name': self.user_name
+        }))
